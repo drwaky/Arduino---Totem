4b145452ed706a051991118101d735ef4fbf374d
Joaquín López <drwaky@gmail.com>
2014-12-18 02:26:17 +0100

    Next version, with more than one working Leds on that will turning off step by step.
    Also a refactor do with testing led and Working / Resting mode variables.

--- arduino-totem.ino
+++ arduino-totem.ino
@@ -8,7 +8,7 @@
  * @bitbucket  drwaky
  * @github     drwaky
  * @copyright  Copyright (c) 2014, DrWaky
- * @version    0.3
+ * @version    0.4
  * @date       2014/12/11
  * @license    GPLv3 - https://www.gnu.org/licenses/gpl-3.0.html
  * @category   Arduino
@@ -16,26 +16,30 @@
  * Arduino Based Pomodoro Totem Creature to the SBA Team at MediaNet Software
  **/
  
- const int restCycle = 4;
- const int defaultLed = 13;
- const int workLed = 2;
- const int restLed = 3;
- int defaultLedState = LOW;
- int workLedState = HIGH;
- int restLedState = LOW;
+ const int restCycle = 4;        // Long rest cycle size
+ const int systemLed = 13;       // Testing Led pin
+ const int restLed = 2;          // Rest Led Arduino pin
+ const int firstWorkLed = 3;     // First Wirk led Arduino pin
+ const int workLedQuantity = 3;  // Quantity of Work Leds
  
- char* mode = "working";
+ boolean working;  // Working mode On or Off
  
- unsigned int pomodorosCount = 0;
+ int currentWorkLedOn = workLedQuantity;  // Quantity of Work Leds are on
+
+ int systemLedState = LOW;    // Testing Led state.
+ int restLedState = LOW;      // Rest Led state.
+ boolean cycleSetSw = false;  // Switch variable to check or not the rest type.
  
- unsigned long defaultLedPreviousMillis = 0;
- unsigned long workRestLedPreviousMillis = 0;
+ unsigned int pomodorosCount = 0;  // Pomodoros counter
  
- const long defaultLedInterval = 500;
- const long shortRestInterval = 500;
- const long longRestInterval = 1500;
- const long workInterval = 3000;
- long restInterval = 0;
+ unsigned long systemLedPreviousMillis = 0;    // Testing Led time moment
+ unsigned long workRestLedPreviousMillis = 0;  // Working or Resting Leds time moment
+ 
+ const long systemLedInterval = 500;  // Testing Led time interval
+ const long shortRestInterval = 500;  // Short rest time interval value in ms
+ const long longRestInterval = 1500;  // Long rest time interval value in ms
+ const long workInterval = 3000;      // Work time interval value in ms
+ long restInterval = 0;               // Corrent Rest time interval in ms (can be long or short)
  
 
 /**
@@ -52,51 +56,85 @@
    }
    return res;
  }
+
+
+/**
+ * Turn on all the work LEDs.
+ * @param      {int} init - The first pin for a work led.
+ * @param      {int} number - The numbers of work led.
+ */
+ void allWorkLedOn(int init, int number){
+   int pin;
+   for (int i = 0; i < number; i++){
+     pin = firstWorkLed + i;
+     digitalWrite(pin, HIGH);
+   }
+ }
  
  
- 
+ /**
+ * Setup the environment.
+ */
  void setup(){
-   pinMode(defaultLed, OUTPUT);
-   pinMode(workLed, OUTPUT);
+   int pin;
+   for (int i = 0; i < workLedQuantity; i++){
+     pin = firstWorkLed + i;
+     pinMode(pin, OUTPUT);
+   }
    pinMode(restLed, OUTPUT);
-   digitalWrite(workLed, workLedState);
+   pinMode(systemLed, OUTPUT);
+   allWorkLedOn(firstWorkLed, workLedQuantity);
+   working = true;
  }
 
 
- 
+ /**
+ * Main infinite loop.
+ */
  void loop(){
    unsigned long currentMillis = millis();
    
-   if(currentMillis - defaultLedPreviousMillis >= defaultLedInterval){
-     defaultLedPreviousMillis = currentMillis;
-     defaultLedState = toggleLedState(defaultLedState);
-     digitalWrite(defaultLed, defaultLedState);
-   }
    
-   if(mode == "working"){
-     if(currentMillis - workRestLedPreviousMillis >= workInterval){
+   /* LED 13th blinking - Only for dev env */
+   if(currentMillis - systemLedPreviousMillis >= systemLedInterval){
+     systemLedPreviousMillis = currentMillis;
+     systemLedState = toggleLedState(systemLedState);
+     digitalWrite(systemLed, systemLedState);
+   }
+   /* --- */
+   
+   
+   if(working){
+     if((currentMillis - workRestLedPreviousMillis >= workInterval / workLedQuantity) && (currentWorkLedOn > 0)){
+       currentWorkLedOn--;
+       workRestLedPreviousMillis = currentMillis;
+       digitalWrite(firstWorkLed + currentWorkLedOn, LOW);
+     } else if(currentWorkLedOn == 0){
        pomodorosCount++;
        workRestLedPreviousMillis = currentMillis;
-       workLedState = toggleLedState(workLedState);
        restLedState = toggleLedState(restLedState);
-       digitalWrite(workLed, workLedState);
        digitalWrite(restLed, restLedState);
-       mode = "resting";
+       working = false;
+       cycleSetSw = false;
      }
-   } else if(mode == "resting"){
-     if(pomodorosCount % restCycle == 0){
-       restInterval = longRestInterval;
-     } else {
-       restInterval = shortRestInterval;
+   } else {
+     if(!cycleSetSw){
+       if(pomodorosCount % restCycle == 0){
+         restInterval = longRestInterval;
+         cycleSetSw = true;
+       } else {
+         restInterval = shortRestInterval;
+         cycleSetSw = true;
+       }
      }
      
      if(currentMillis - workRestLedPreviousMillis >= restInterval){
        workRestLedPreviousMillis = currentMillis;
-       workLedState = toggleLedState(workLedState);
+       allWorkLedOn(firstWorkLed, workLedQuantity);
+       currentWorkLedOn = workLedQuantity;
        restLedState = toggleLedState(restLedState);
-       digitalWrite(workLed, workLedState);
        digitalWrite(restLed, restLedState);
-       mode = "working";
+       working = true;
      }
    }
  }
